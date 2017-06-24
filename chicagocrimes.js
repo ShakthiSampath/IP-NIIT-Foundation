bbb703a8c63cf2f3ac312d8ad1fb7a16fd4a88db
Shakthi <shakthivvbhss@gmail.com>
2017-06-24 23:46:51 +0530

    Exception Handling done in chicagocrimes.js

--- chicagocrimes.js
+++ chicagocrimes.js
@@ -13,53 +13,94 @@ let year;
 let description;
 let primaryType;
 let newobj = [];
+var createfile = true;
+const re = /,(?=(?:(?:[^"]*"){2})*[^"]+$)/;
 
-const rl = readline.createInterface({
-    input: fs.createReadStream('../Material/chicagocrimes.csv')
-    // input: fs.createReadStream('../Material/chicagocrimes_3.csv')
-});
 
-rl.on('line', (line) => {
+// paths created
 
+const path = '../Material/chicagocrimes.csv';
+// const path1 = '../Material/chicagocrimes_3.csv';   // this path is for checking whether it would allow the improper format in csv data
+
+//checking the path
+
+if(!fs.existsSync(path)){
+
+    console.log('The file you are trying to access is not available on the given path!');
+    console.log('Please provide an appropriate path!');
+
+}
+else{
+
+    const rl = readline.createInterface({
+
+    input: fs.createReadStream(path)
+    // input: fs.createReadStream(path1)
+
+    });
     
-    //2. extract header row
-    line = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);    
-    if (count === 0) {
-        year = line.indexOf('Year');
-        description = line.indexOf('Description');
-        primaryType = line.indexOf('Primary Type');
-        count++;
-    }       
 
-    //3. filter value according graph necesstities
-    if(line[primaryType] === 'THEFT')
-    {var flag =0;
-         newobj.map(function(element){
-        if(line[year] === element['Year'] ){
-                    if(line[description] === 'OVER $500')
-                    {
-                        element['OVER $500'] += 1;
+        rl.on('line', (line) => {
+
+          // checks whether the format inside csv is correct or not
+            if(line.split(',') == false){
+
+                 createfile = false;
+                 return;    
+                 
+            }  
+            else{
+
+                line = line.split(re);  
+
+                if (count === 0) {
+                    year = line.indexOf('Year');
+                    description = line.indexOf('Description');
+                    primaryType = line.indexOf('Primary Type');
+                    count++;
+                }       
+
+                //3. filter value according graph necesstities
+                if(line[primaryType] === 'THEFT')
+                {
+                    var flag =0;
+                    newobj.map(function(element){
+                    if(line[year] === element['Year'] ){
+                                if(line[description] === 'OVER $500')
+                                {
+                                    element['OVER $500'] += 1;
+                                }
+                                else if(line[description] === '$500 AND UNDER')
+                                {
+                                    element['$500 AND UNDER'] += 1;
+                                }
+                                flag = 1;
                     }
-                    else if(line[description] === '$500 AND UNDER')
-                    {
-                        element['$500 AND UNDER'] += 1;
+                    });
+                    if(flag == 0){
+                            if(line[description] === 'OVER $500')
+                                newobj.push({"OVER $500": 1,"$500 AND UNDER": 0,"Year": line[year]})
+                            else if(line[description] === '$500 AND UNDER')
+                                newobj.push({"OVER $500": 0,"$500 AND UNDER": 1,"Year": line[year]})
                     }
-                    flag = 1;
-        }
-        });
-        if(flag == 0){
-                if(line[description] === 'OVER $500')
-                    newobj.push({"OVER $500": 1,"$500 AND UNDER": 0,"Year": line[year]})
-                else if(line[description] === '$500 AND UNDER')
-                    newobj.push({"OVER $500": 0,"$500 AND UNDER": 1,"Year": line[year]})
-        }
-                  
+                            
+                }
+        
+            }
+    }).on('close', () => { // whenever the reading of the file is completed it would execute this close event
+
+    if(createfile === false)
+    {
+        console.log('The data inside the file is not comma seperated!');
+        console.log('Hence, JSON File could not be written!');
     }
-    
-
-}).on('close', () =>{
-   writemyfile(newobj);
+    else{
+        writemyfile(newobj);
+        console.log('JSON File has been successfully written!');
+    }
 })
+
+}
 
 // function to write a file
 
@@ -70,5 +111,4 @@ function writemyfile(thearray){
             if (err) throw err;
 
      });
-
 }
