9af4adf1152697d9900ba164780cea1c836d5a33
Shakthi <shakthivvbhss@gmail.com>
2017-06-27 23:27:42 +0530

    Updated Files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 const PORT=8080; 
 
-fs.readFile('./chicagocrimesD3.html', function (err, html) {
+fs.readFile('../Learning/index2.html', function (err, html) {
 
     if (err) throw err;    
 
